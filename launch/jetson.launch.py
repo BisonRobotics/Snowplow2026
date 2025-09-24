7248f027950d930cc7e931547292d04e998b5732
LegoMaster7001 <superminr21@gmail.com>
2025-09-24 18:48:02 -0500

    Refactor apriltag parameters to use ROS parameters instead of hardcoded values

--- launch/jetson.launch.py
+++ launch/jetson.launch.py
@@ -6,6 +6,14 @@ def generate_launch_description():
         Node(
             package='jetson_pkg',
             executable='apriltag',
-            name='apriltag'
+            name='apriltag',
+            parameters=[
+                {'cap':'rtsp://admin:hyflex@192.168.1.131:80/cam/realmonitor?channel=1&subtype=0'},
+                {'fx':1071.1362274102335},
+                {'fy':1102.1406887400624},
+                {'cx':953.030188084331},
+                {'cy':468.0382502048589}
+
+            ]
         )
     ])
--- src/jetson_pkg/jetson_pkg/jetson_apriltag.py
+++ src/jetson_pkg/jetson_pkg/jetson_apriltag.py
@@ -10,10 +10,6 @@ import threading
 import cv2
 import apriltag
 
-cap = cv2.VideoCapture('rtsp://admin:hyflex@192.168.1.131:80/cam/realmonitor?channel=1&subtype=0')
-detector = apriltag.Detector()
-fx, fy, cx, cy = (1071.1362274102335, 1102.1406887400624, 953.030188084331, 468.0382502048589)
-
 class ApriltagPublisher(Node):
     def __init__(self):
         super().__init__('apriltag_publisher')
@@ -22,18 +18,31 @@ class ApriltagPublisher(Node):
         self.latest_frame = None
         threading.Thread(target=self.keep_up_thread).start()
         self.timer = self.create_timer(timer_period, self.timer_callback)
+        #declare parameters
+        self.detector = apriltag.Detector()
+        self.declare_parameter('cap')
+        self.declare_parameter('fx')
+        self.declare_parameter('fy')
+        self.declare_parameter('cx')
+        self.declare_parameter('cy')
+        #assign parameters
+        self.cap = cv2.VideoCapture(self.get_parameter('cap').value)
+        self.fx = self.get_parameter('fx').value
+        self.fy = self.get_parameter('fy').value
+        self.cx = self.get_parameter('cx').value
+        self.cy = self.get_parameter('cy').value
 
     def keep_up_thread(self):
         while True:
-            success, frame = cap.read()
+            success, frame = self.cap.read()
             if success:
                 self.latest_frame = frame
 
     def timer_callback(self):
         gray = cv2.cvtColor(self.latest_frame, cv2.COLOR_RGB2GRAY)
-        detections = detector.detect(gray)
+        detections = self.detector.detect(gray)
         if len(detections) > 0:
-            pose, _, _ = detector.detection_pose(detections[0], [fx,fy,cx,cy], 0.3254375)
+            pose, _, _ = self.detector.detection_pose(detections[0], [self.fx,self.fy,self.cx,self.cy], 0.3254375)
             relative_x = pose[0][3] + -0.017
             relative_z = pose[2][3] + 0.83
             relative_rotation = np.arcsin(-pose[2][0]) * (180 / math.pi)
