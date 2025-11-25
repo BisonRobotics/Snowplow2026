b7b5b9463e2d6d3807e974f308a17e0893d6642a
Berma985 <benjkordo@outlook.com>
2025-11-24 19:00:27 -0600

    Added launch file for lidar
    It works now!!!

--- launch/lidar.launch.py
+++ launch/lidar.launch.py
@@ -1,32 +1,25 @@
+from launch_ros.actions import Node
+from launch import LaunchDescription
 import os
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch_ros.actions import Node
 
 def generate_launch_description():
     sick_scan_pkg_prefix = get_package_share_directory('sick_scan_xd')
     lms_launch_file_path = os.path.join(sick_scan_pkg_prefix, 'launch/sick_lms_1xx.launch')
-    laser_scan_node = Node(
+    laserscan_node = Node(
         package='sick_scan_xd',
         executable='sick_generic_caller',
         output='screen',
         arguments=[
             lms_launch_file_path,
+            #'nodename:=laserscan1',
             'hostname:=192.168.1.134',
             'tf_base_frame_id:=lidar_laser',
             'scan_freq:=25',
             'ang_res:=0.5',
-            ]
+            #'cloud_topic:=cloud1a',
+            #'frame_id:=laserscan1/lidardata'
+        ]
     )
-    
-    obs_node = Node(
-        package='sensors_pkg',
-        executable='obs',
-        name='obs',
-    )
-    
     return LaunchDescription([
-        laser_scan_node,
-        obs_node
-    ])
-
+        laserscan_node])
