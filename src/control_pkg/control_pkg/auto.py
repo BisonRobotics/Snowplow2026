3df8c739473fa458d0308e652455d28fe60a62ae
Garret B <burnightgm@gmail.com>
2025-12-08 18:16:00 -0600

    New Auto Path

--- src/control_pkg/control_pkg/auto.py
+++ src/control_pkg/control_pkg/auto.py
@@ -5,9 +5,10 @@ from std_msgs.msg import Float32, Int8
 from geometry_msgs.msg import Twist
 
 from control_pkg.commands import Runner, Command
-from control_pkg.drive_commands import DriveDistanceCommand, DriveToWaypointCommand
+from control_pkg.drive_commands import DriveDistanceCommand, DriveToWaypointCommand, DriveBackwardsToWaypointCommand
 from control_pkg.wait_commands import WaitCommand, WaitUntilCommand
 from control_pkg.turn_command import TurnToDegreesCommand
+from control_pkg.cone_zone_detection import find_zone 
 
 class Auto(Node):
     def __init__(self):
@@ -33,6 +34,405 @@ class Auto(Node):
         # Start auto command
         self.runner.start_command(self.get_auto_command())
         
+    def get_auto_command_2(self) -> Command:
+        """
+        Generates and returns the command to start when auto is started
+        !!!! This code has not been tested yet !!!!
+        !!!! need to read the obstacle data from obstical ovidance branch instead of hard coded cone location !!!!
+        
+
+        Returns:
+            Command: Command to start when auto is started
+        """
+        # Waypoint after left turn out
+        left_waypoint = Twist()
+        left_waypoint.linear.x = -2.5
+        left_waypoint.linear.z = 2
+        left_waypoint.angular.y = 180
+        
+        # Waypoint after right turn out
+        right_waypoint = Twist()
+        right_waypoint.linear.x = 2.5
+        right_waypoint.linear.z = 2
+        right_waypoint.angular.y = 0
+
+        # Garage Waypoint
+        garage_waypoint = Twist()
+        garage_waypoint.linear.x = 0
+        garage_waypoint.linear.z = -1.2
+        garage_waypoint.angular.y = 90
+
+        waypoint1, waypoint2, waypoint3, waypoint4, waypoint5, waypoint6, waypoint7, waypoint8 = Twist()
+        waypoint_array = [waypoint1, waypoint2, waypoint3, waypoint4, waypoint5, waypoint6, waypoint7, waypoint8]
+        i = 1
+        for waypoint in waypoint_array:
+            waypoint.linear.x = 2*(-(i%2)+0.5)*(5-((i+i%2)/2)) - (i%2) + 0.5
+            waypoint.linear.z = 2
+            waypoint.angular.y = 90
+            i += 1
+        
+        
+        # Factory functions for removing redundancy
+        wait = lambda time_seconds : WaitCommand(time_seconds)
+        turn_to_degrees = lambda degrees : TurnToDegreesCommand(degrees, self.get_pivot_position, self.drive_pivot)
+        drive_to_waypoint = lambda waypoint : DriveToWaypointCommand(waypoint, self.get_position, self.get_pivot_position, self.drive_pivot, self.drive)
+        drive_backwards_to_waypoint = lambda waypoint : DriveBackwardsToWaypointCommand(waypoint, self.get_position, self.get_pivot_position, self.drive_pivot, self.drive)
+        drive_distance = lambda speed, distance : DriveDistanceCommand(speed, distance, self.drive)
+        wait_until = lambda condition : WaitUntilCommand(condition)
+
+        # Creating segments
+        ConeInternal = find_zone(1.5, 2) # Filler until we get Lidar Data.
+        ConeExternal = find_zone(1.5, 2) # Filler until we get Lidar Data.
+        
+        TurningAngle = 18.624 
+
+        #Segment 1
+        if(ConeInternal < 8.5):
+            # Path A
+            segment1 = turn_to_degrees(TurningAngle)\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(-TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 2.75))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 2.75))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(-TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 0.2))\
+                    .and_then(wait(4))\
+                    .and_then(turn_to_degrees(-TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 2.75))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 2.75))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(-TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 0.2))
+            
+        elif (ConeInternal >8.5):
+            if(ConeInternal%2 ==1 ):
+                # Path B Right
+                segment1 = turn_to_degrees(TurningAngle)\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(-TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 2.75))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 2.75))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(-TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 0.2))
+            else:
+                # Path B Left
+                segment1 = turn_to_degrees(-TurningAngle)\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(1, 2.75))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 2.75))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 0.2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(-TurningAngle))\
+                    .and_then(wait(2))\
+                    .and_then(drive_distance(-1, 0.2))
+            
+            
+        # Segment 2 
+        if((ConeExternal == -7) and (ConeInternal%2 == 1)):
+            #Path D Right
+            segment2 = turn_to_degrees(0)\
+                .and_then(wait(2))\
+                .and_then(drive_to_waypoint(right_waypoint))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(0))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(1, 2))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(-22))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(1, 3.14 * 1.90475 / 4))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(-1, 3.14 * 1.90475 / 4))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(0))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(-1, 1.5))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(18.624))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(-1, 2.25 * 3.14 / 2))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(0))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(-1, 1.5))
+        elif((ConeExternal == -1) and (ConeInternal%2 == 0)):
+          # Path D Left
+            segment2 = turn_to_degrees(0)\
+                .and_then(wait(2))\
+                .and_then(drive_to_waypoint(left_waypoint))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(0))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(1, 2))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(-22))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(1, 3.14 * 1.90475 / 4))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(-1, 3.14 * 1.90475 / 4))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(0))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(-1, 1.5))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(-18.624))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(-1, 2.25 * 3.14 / 2))\
+                .and_then(wait(2))\
+                .and_then(turn_to_degrees(0))\
+                .and_then(wait(2))\
+                .and_then(drive_distance(-1, 1.5))
+        else:
+           if(ConeInternal%2 == 1):
+               # Path C Right
+                segment2 = turn_to_degrees(0)\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint8))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint6))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint4))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+           else:
+               # Path C Left
+                segment2 = turn_to_degrees(0)\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint7))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint5))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint3))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint1))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+        
+        #Segment 3
+        if((ConeExternal == -1 and ConeInternal%2 == 1) or (ConeExternal == -7 and ConeInternal%2 == 0)):
+            # Path X # More Conditional Logic tobe developed.
+            segment3 = turn_to_degrees(0).and_then(drive_distance(1, 2)) # PlaceHolder
+        elif(ConeInternal%2 == 1):
+            '''
+            This code will build segment 3 with each path, 
+            leaving out the square where the cone is located.
+            '''
+            if(not(ConeInternal == 8)):
+                # Path C.1 Right
+                segment3 = segment3\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint8))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+            if(not(ConeInternal == 6)):
+                # Path C.2 Right
+                segment3 = segment3\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint6))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+            if(not(ConeInternal == 4)):
+                # Path C.3 Right
+                segment3 = segment3\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint4))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+            if(not(ConeInternal == 2)):
+                # Path C.4 Right
+                segment3 = segment3\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint2))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+        else:
+            '''
+            This code will build segment 3 with each path, 
+            leaving out the square where the cone is located.
+            '''
+            if(not(ConeInternal == 7)):
+                # Path C.1 Left
+                segment3 = segment3\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint7))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+            if(not(ConeInternal == 5)):
+                # Path C.2 Left
+                segment3 = segment3\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint5))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+            if(not(ConeInternal == 3)):
+                # Path C.3 Left
+                segment3 = segment3\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint3))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+            if(not(ConeInternal == 1)):
+                # Path C.4 Left
+                segment3 = segment3\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_to_waypoint(waypoint1))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))\
+                    .and_then(wait(2))\
+                    .and_then(drive_backwards_to_waypoint(garage_waypoint))\
+                    .and_then(wait(2))\
+                    .and_then(turn_to_degrees(0))
+
+
+        
+         
+        # Creating and returning command
+        return wait_until(lambda : self.position_updated and self.pivot_position_updated)\
+                .and_then(segment1)\
+                .and_then(wait(2))\
+                .and_then(segment2)\
+                .and_then(wait(2))\
+                .and_then(segment3)\
+
+
+
     def get_auto_command(self) -> Command:
         """
         Generates and returns the command to start when auto is started
@@ -73,7 +473,7 @@ class Auto(Node):
                 .and_then(wait(2))\
                 .and_then(drive_distance(1, 3.14 * 1.90475 / 4))\
                 .and_then(wait(2))\
-                .and_then(drive_distance(-1, 3.14 * 1.90471 / 4))\
+                .and_then(drive_distance(-1, 3.14 * 1.90475 / 4))\
                 .and_then(wait(2))\
                 .and_then(turn_to_degrees(0))\
                 .and_then(wait(2))\
---
+++ src/control_pkg/control_pkg/cone_zone_detection.py
@@ -0,0 +1,55 @@
+
+def find_zone(x,y) -> int:
+    # Zones are numbered Externally towards the inside with, .
+    # Exterial Zones are numbered -1 to -7 clockwise starting from the garage. 
+    # Returns 0 if Cone is outside of all zones.
+
+    # Checking for Cone in one of the 14 Squares
+    # Top Left Squares
+    if -5 < x <= -4 and 1.5 < y < 2.5:
+        return 1 # Square 1 
+    elif -4 < x <= -3 and 1.5 < y < 2.5:
+        return 3 # Square 3
+    elif -3 < x <= -2 and 1.5 < y < 2.5:
+        return 5 # Square 5
+    elif -2 < x <= -1 and 1.5 < y < 2.5: 
+        return 7 # Square 7
+    elif -1 < x <=  0 and 1.5 < y < 2.5: 
+        return 9 # Square 9
+    # Top Right Squares
+    elif  0 < x <=  1 and 1.5 < y < 2.5:
+        return 10 # Square 10 
+    elif  1 < x <=  2 and 1.5 < y < 2.5:
+        return 8 # Square 8
+    elif  2 < x <=  3 and 1.5 < y < 2.5:
+        return 6 # Square 6
+    elif  3 < x <=  4 and 1.5 < y < 2.5: 
+        return 4 # Square 4
+    elif  4 < x <= 5 and 1.5 < y < 2.5: 
+        return 2 # Square 2
+    # Bottom Four Squares 
+    elif -1 < x <=  0 and 0.75 < y < 1.5:
+        return 11 # Square 11
+    elif  0 < x <=  1 and 0.75 < y < 1.5:
+        return 12 # Square 12
+    elif  -1 < x <= 0 and 0.00 < y < 0.75:
+        return 13 # Square 13  
+    elif  0 < x <= 1 and 0.00 < y < 0.75:
+        return 14 # Square 14
+    # Checking for Cone in outer Zones.
+    elif -5 < x <= -1 and 0 < y < 1.5:
+        return -1 # Zone Left of Garage.
+    elif -8 < x <= -5 and 0 < y < 4:
+        return -2 # Left Manuvering Zone
+    elif -5 < x <= -1 and 2.5 < y < 4:
+        return -3 # Far Left Zone.
+    elif -1 < x <= 1 and 2.5 < y < 4:
+        return -4 # Evil Zone thats directly ahead of starting,
+    elif 1 < x <= 5 and 2.5 < y < 4:
+        return -5 # Far Right Zone
+    elif 5 < x <= 8 and 0 < y < 4:
+        return -6 # Far Right Manuevering Zone
+    elif 1 < x <= 5 and 0 < y < 1.5: 
+        return -7 # Zone Right of Garage
+    else:
+        return 0 # Outside of playfield.
