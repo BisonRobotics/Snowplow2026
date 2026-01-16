00a5ec8db2c9bc4518ac2c144483a4ba1f019484
Kaitlyn Nickel <nickelk102703@gmail.com>
2026-01-15 23:50:45 -0600

    Fixed tag_id and removed old auto publisher

--- src/jetson_pkg/jetson_pkg/jetson_apriltag.py
+++ src/jetson_pkg/jetson_pkg/jetson_apriltag.py
@@ -9,6 +9,7 @@ import numpy as np
 import threading
 import cv2
 import apriltag
+#locations of the apriltag in x,z,theta
 poses = {
     14: (0,3.71,270),
     12: (-7.71,2,0),
@@ -17,7 +18,6 @@ poses = {
 class ApriltagPublisher(Node):
     def __init__(self):
         super().__init__('apriltag_publisher')
-        self.oldpublisher_ = self.create_publisher(Twist, '/old_apriltag', 10)
         self.publisher_ = self.create_publisher(Twist, '/apriltag', 10)
         timer_period = 0.5
         self.latest_frame = None
@@ -63,7 +63,8 @@ class ApriltagPublisher(Node):
             gray = cv2.cvtColor(self.latest_frame, cv2.COLOR_RGB2GRAY)
             detections = self.detector.detect(gray)
             if len(detections) > 0:
-                pose, tag_id, _ = self.detector.detection_pose(detections[0], [self.fx, self.fy, self.cx, self.cy], 0.3254375)
+                pose, _, _ = self.detector.detection_pose(detections[0], [self.fx, self.fy, self.cx, self.cy], 0.3254375)
+                tag_id = detections[0].tag_id
                 relative_x = pose[0][3] + -0.017
                 relative_z = pose[2][3] + 0.83
                 relative_rotation = np.arcsin(-pose[2][0]) * (180 / math.pi)
@@ -73,19 +74,7 @@ class ApriltagPublisher(Node):
                 msg.linear.x = xr
                 msg.linear.z = zr
                 msg.angular.y = thetar
-                self.oldpublisher_.publish(msg)
-                #new without math
-                relative_x = pose[0][3]
-                relative_y = pose[1][3]
-                relative_z = pose[2][3]
-
-                msg_no_offset = Twist()
-                msg_no_offset.linear.x = relative_x
-                msg_no_offset.linear.z = relative_z
-                msg_no_offset.linear.y = relative_y
-                
-                self.get_logger().debug(f'{msg_no_offset}')
-                self.publisher_.publish(msg_no_offset)
+                self.publisher_.publish(msg)
         except cv2.error as e:
             self.get_logger().error(f"OpenCV error: {e}")
         
